b2d8eef9e33918c63d596ffeed8739db9c005d88
Tom Hoover <tom@hisword.net>
2022-11-12 17:31:17 -0600

    Write all items to all.txt

--- amazon-pricedrop.py
+++ amazon-pricedrop.py
@@ -5,6 +5,8 @@ from time import sleep
 import csv
 import random
 import requests
+import os
+import sys
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 44.0.2403.157 Safari / 537.36",
@@ -13,12 +15,15 @@ HEADERS = {
 
 
 def main():
-    with open("purchases.csv", "r") as infile, open("price_drops.txt", "w") as outfile:
+    # fmt:off
+    with open("purchases.csv", "r") as infile, open("price_drops.txt", "w") as outfile, open("all.txt", "w") as allfile:
+    # fmt:off
         items = csv.DictReader(infile)
         item_list = []
         for item in items:
             item_list.append(item)
 
+        allfile.write("url,cost,current_price,date,desc\n")
         outfile.write("url,cost,current_price,date,desc\n")
 
         for item in item_list:
@@ -29,19 +34,37 @@ def main():
                 item["Purchase Price Per Unit"].replace("$", "").replace(",", "")
             )
             url = "https://www.amazon.com/dp/" + asin
+
+            if os.isatty(sys.stdout.fileno()):
+                print(f"  * Downloading {desc}")
+
             page = requests.get(url, headers=HEADERS)
             soup = BeautifulSoup(page.content, "lxml")
 
-            price = float(
-                soup.find("span", attrs={"class": "a-offscreen"})
-                .string.strip()
-                .replace("$", "")
-                .replace(",", "")
-            )
+            if desc == "":
+                desc = soup.find(id="productTitle").get_text().strip()
+                # print(desc)
 
-            if price < cost:
-                outfile.write(f"{url},{cost:.2f},{price:.2f},{date},{desc}\n")
-                print(f"{url},{cost:.2f},{price:.2f},{date},{desc}")
+            price = ""
+            try:
+                price = float(
+                    soup.find("span", attrs={"class": "a-offscreen"})
+                    .string.strip()
+                    .replace("$", "")
+                    .replace(",", "")
+                )
+            except ValueError as err:
+                print(f"ValueError: {err}")
+                price = 0
+            except AttributeError:
+                price = "N/A"
+
+            allfile.write(f"{url},{cost:.2f},{price},{date},{desc}\n")
+
+            if type(price) == str or price < cost:
+                outfile.write(f"{url}\t{cost:.2f}\t{price}\t{date}\t{desc}\n")
+                if os.isatty(sys.stdout.fileno()):
+                    print(f"{url}, {cost}, {price}, {date}, {desc}")
 
             sleep(random.randint(1, 10))
 
